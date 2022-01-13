256aa677a8fccae1a232f613235f414a9c1c7617
Dave <d2cb103@gmail.com>
2022-01-14 02:13:29 +0800

    step21

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,54 @@ class UsersController < ApplicationController
 
     before_action :find_user, only: %i[edit show]
 
-    def idnex 
+    def index 
         @users = User.all
+        @users = @users.order(id: :ASC).page(params[:page]).per(9)
     end
 
     def new
         @user = User.new
     end
     
+    def show
+    
+        @q = User.find(params[:id]).tasks.ransack(params[:q])
+        @tasks = @q.result
+        @tasks = @tasks.order('id ASC').page(params[:page]).per(9)
+
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to '/tasks' , notice: t('notice.new')
+            redirect_to admin_path , notice: t('notice.new')
         else 
             render :new
+        end
+    end
+
+    def edit
+        @user = User.find_by(id: params[:id])
+    end
+    
+    def destroy
+        @user = User.find_by(id: params[:id])
+        @tasks = @user.tasks
+        @tasks.destroy_all if @tasks
+        @user.destroy if @user
+        
+        redirect_to admin_path, notice: I18n.t('notice.delete')
+    end
+
+
+    def update
+        @user = User.find_by(id: params[:id])
+        if @user.update(user_params)
+            redirect_to admin_path
+            flash[:notice] = I18n.t('notice.update')
+        else
+            flash[:notice] = @user.errors.full_messages.to_sentence
+            render :edit
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :tasks
-  # resources :users
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
 
   #session
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   #user
+  get 'admin' ,to: 'users#index'
   get 'sign_up', to: 'users#new'
   resources :users, except: [:new]
   
